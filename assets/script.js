3ad21924b99617769c170d066abf6cef49aadcbe
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-01-17 16:42:33 -0500

    IP lookup and image change

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,39 @@
+var q = "https://json.geoiplookup.io/"
+var userCity = '';
+
+axios.get(q)
+    .then(res => {
+        $('h1').text(`${res.data.city}, ${res.data.region}`)
+        userCity = res.data.city.toLowerCase();
+        var newstr = "";
+        for (var i = 0; i < userCity.length; i++) {
+            if (userCity[i] === " ") {
+                newstr += "-";
+            } else {
+                newstr += userCity[i]
+            }
+        }
+        q = `https://api.teleport.org/api/urban_areas/slug:${res.data.city.toLowerCase()}/images/`;
+        axios.get(q)
+            .then(res => {
+                console.log(res);
+                var backImage = res.data.photos[0].image.web;
+                $('#cityHero').css('background-image', `url("${backImage}"`);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    })
+    .catch(e => {
+        console.log(e);
+    })
+
 $('.ui.accordion').accordion();
+
+$('.delete').click(function() {
+    $($(this).parent().parent()).remove()
+})
+
+$('.label').click(function() {
+    $('#cityInput').val($(this).text().trim())
+})
